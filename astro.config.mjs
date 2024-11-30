56eb7197751b013cccb663b430119360e563bb78
PascalStehling <pascal.stehling@catenion.com>
2024-11-30 11:59:18 +0000

    feat: :sparkles: add vercel web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,7 @@ import remarkToc from "remark-toc";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://stehl.ing",
+  site: "https://pascal.stehl.ing",
   integrations: [tailwind(), sitemap(), mdx({
     shikiConfig: {
       theme: "css-variables",
